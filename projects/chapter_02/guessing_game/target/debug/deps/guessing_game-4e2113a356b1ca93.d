42d629facd92c5bf229483f86c713fb4691ec2dc
nka21 <naoki.nico@gmail.com>
2024-08-04 21:52:04 +0900

    build: cargo run

---
+++ projects/chapter_02/guessing_game/target/debug/deps/guessing_game-4e2113a356b1ca93.d
@@ -0,0 +1,5 @@
+/Users/naoki/coding/rust-tutorial/projects/chapter_02/guessing_game/target/debug/deps/guessing_game-4e2113a356b1ca93: src/main.rs
+
+/Users/naoki/coding/rust-tutorial/projects/chapter_02/guessing_game/target/debug/deps/guessing_game-4e2113a356b1ca93.d: src/main.rs
+
+src/main.rs:
---
+++ projects/chapter_02/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/naoki/coding/rust-tutorial/projects/chapter_02/guessing_game/target/debug/guessing_game: /Users/naoki/coding/rust-tutorial/projects/chapter_02/guessing_game/src/main.rs
