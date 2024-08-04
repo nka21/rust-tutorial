983c3285804b92f261101d3cca18864ba24c4cd1
nka21 <naoki.nico@gmail.com>
2024-08-04 22:16:23 +0900

    chore: オウム返しプログラムの作成
    標準入力の実装
    入力の値を出力するプログラム
    
    input: 7
    output: 7

--- projects/chapter_02/guessing_game/src/main.rs
+++ projects/chapter_02/guessing_game/src/main.rs
@@ -1,3 +1,29 @@
+// 標準入出力を行うための io というライブラリ
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    /*
+        let apples = 5; // 不変
+        let mut bananas = 5; // 可変
+    */
+
+    let mut guess = String::new(); // String型の新しいインスタンスを作成
+
+
+    io::stdin()
+        .read_line(&mut guess) // 入力をguessに格納
+        .expect("Failed to read line"); // 入力処理に対するエラー処理
+
+    /*
+        let x = 5;
+        let y = 10;
+
+        println!("x = {} and y = {}", x, y); // x = 5 and y = 10
+    */
+
+    println!("You guessed: {}", guess); // {}に、第２引数のguessが入る
 }
